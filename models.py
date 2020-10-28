357d94a45ce20896d7ff19179842fdf938f0655f
anushribarsaiyan <53507782+anushribarsaiyan@users.noreply.github.com>
2020-10-28 11:46:12 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class user(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    real_name = models.CharField(max_length=70)
+    tz = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.real_name
+
+
+class activity(models.Model):
+    user_activity = models.ForeignKey(user, related_name='user_activities', on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.user_activity
+
